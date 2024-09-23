f9dd8ddc5158530ddc67523f9abdc7de3b523237
Vinitx67 <vinitvishwakarma67@gmail.com>
2024-09-24 01:08:32 +0530

    changes

---
+++ PROJECT/project-header.h
@@ -0,0 +1,218 @@
+#include <iostream>
+using namespace std;
+
+#define MIN_BALANCE 1500
+#define MAX_ACCOUNTS 100
+
+class Account
+{
+private:
+    int accNumber;
+    string name;
+    double balance;
+    int indexNum;
+
+public:
+    Account()
+    {
+        // DEFAULT CONSTRUCTOR
+    }
+    Account(int accNumber, string name, double balance)
+    {
+        // PARAMETERIZED CONSTRUCTOR
+        this->accNumber = accNumber;
+        this->name = name;
+        this->balance = balance;
+    }
+
+    // UTILITY FUNCTIONS
+    void deposit(int amount)
+    {
+        balance = balance + amount;
+    }
+    void withdraw(int amount)
+    {
+        balance = balance - amount;
+    }
+
+    // GETTERS AND SETTERS
+    int getAccNumber()
+    {
+        return accNumber;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    double getBalance()
+    {
+        return balance;
+    }
+
+    int getIndexNum()
+    {
+        return indexNum;
+    }
+
+    void setName(string name)
+    {
+        this->name = name;
+    }
+
+    void setIndexNum(int indexNum)
+    {
+        this->indexNum = indexNum;
+    }
+
+    void setBalance(int balance)
+    {
+        this->balance = balance;
+    }
+
+    void displayData()
+    {
+        cout << "ACCOUNT NUMBER : " << accNumber << endl;
+        cout << "ACCOUNT NAME : " << name << endl;
+        cout << "ACCOUNT BALANCE : " << balance << endl;
+        // cout << "INDEX NUMBER : " << indexNum << endl;
+    }
+};
+class Bank
+{
+private:
+    // ACOUNTS
+    Account accounts[MAX_ACCOUNTS];
+
+public:
+    static int accNum;
+    static int index;
+    void createAccount(string name, double amount)
+    {
+        if (index < MAX_ACCOUNTS)
+        {
+            if (amount >= MIN_BALANCE)
+            {
+                Account accountObj(accNum, name, amount);
+                accountObj.setIndexNum(index);
+                accounts[index] = accountObj;
+                cout << "ACCOUNT CREATED SUCCESSFULLY !!!\n";
+                accountObj.displayData();
+                accNum++;
+                index++;
+            }
+            else
+            {
+                cout << "PLEASE ENTER AMOUNT MORE THAN " << MIN_BALANCE << " !!!" << endl;
+            }
+        }
+        else
+        {
+            cout << "CURRENTLY OUT OF SERVICE !!! \n";
+        }
+    }
+
+    Account getAccountByAccNum(int accountNumber)
+    {
+        // SEARCH FROM ARRAY
+        for (int i = 0; i < index; i++)
+        {
+            if (accounts[i].getAccNumber() == accountNumber)
+            {
+                return accounts[i];
+            }
+        }
+        Account dummyAccount;
+        dummyAccount.setIndexNum(-1);
+        return dummyAccount;
+    }
+
+    void depositAmount(double amount, int accountNumber)
+    {
+        Account existingAccount = getAccountByAccNum(accountNumber);
+        if (existingAccount.getIndexNum() == -1)
+        {
+            cout << "ACCOUNT WITH ACCOUNT NUMBER " << accountNumber << " DOESN'T EXISTS !!!\n";
+        }
+        else
+        {
+            existingAccount.deposit(amount);
+            int existingAccountIndex = existingAccount.getIndexNum();
+            accounts[existingAccountIndex] = existingAccount;
+            cout << "AMOUNT " << amount << " CREDITED SUCCESSFULLY AND THE AVAILABLE BALANCE IS " << existingAccount.getBalance() << endl;
+        }
+    }
+
+    void withdrawAmount(double amount, int accountNumber)
+    {
+        Account existingAccount = getAccountByAccNum(accountNumber);
+        if (existingAccount.getIndexNum() == -1)
+        {
+            cout << "ACCOUNT WITH ACCOUNT NUMBER " << accountNumber << " DOESN'T EXISTS !!!\n";
+        }
+        else
+        {
+            double existingAmount = existingAccount.getBalance();
+            if (existingAmount - amount >= MIN_BALANCE)
+            {
+                existingAccount.withdraw(amount);
+                int existingAccountIndex = existingAccount.getIndexNum();
+                accounts[existingAccountIndex] = existingAccount;
+                cout << "AMOUNT " << amount << " DEBITED SUCCESSFULLY AND THE AVAILABLE BALANCE IS " << existingAccount.getBalance() << endl;
+            }
+            else
+            {
+                cout << "INSUFFICIENT BALANCE. YOUR BALANCE IS " << existingAmount << " YOU ARE TRYING TO WITHDRAW AMOUNT " << amount << endl;
+            }
+        }
+    }
+
+    void transferAmount(int fromAccountNumber, int toAccountNumber, double amount)
+    {
+        // Retrieve the accounts for both the sender and the receiver
+        Account fromAccount = getAccountByAccNum(fromAccountNumber);
+        Account toAccount = getAccountByAccNum(toAccountNumber);
+
+        // Check if both accounts exist
+        if (fromAccount.getIndexNum() == -1)
+        {
+            cout << "SOURCE ACCOUNT WITH ACCOUNT NUMBER " << fromAccountNumber << " DOESN'T EXISTS !!!\n";
+        }
+        if (toAccount.getIndexNum() == -1)
+        {
+            cout << "DESTINATION ACCOUNT WITH ACCOUNT NUMBER " << toAccountNumber << " DOESN'T EXISTS !!!\n";
+        }
+
+        // Check if there is enough balance in the source account
+        if (fromAccount.getBalance() - amount >= MIN_BALANCE)
+        {
+            // Perform the transfer: debit from source, credit to destination
+            fromAccount.withdraw(amount);
+            toAccount.deposit(amount);
+
+            // Update the accounts in the array
+            accounts[fromAccount.getIndexNum()] = fromAccount;
+            accounts[toAccount.getIndexNum()] = toAccount;
+
+            cout << "AMOUNT " << amount << " TRANSFERRED SUCCESSFULLY FROM ACCOUNT " << fromAccountNumber << " TO ACCOUNT " << toAccountNumber << endl;
+            cout << "REMAINING BALANCE IN SOURCE ACCOUNT " << fromAccountNumber << " IS : " << fromAccount.getBalance() << endl;
+        }
+        else
+        {
+            cout << "INSUFFICIENT BALANCE IN SOURCE ACCOUNT. CURRENT BALANCE IS : " << fromAccount.getBalance() << " YOU ARE TRYING TO TRANSFER AMOUNT " << amount << endl;
+        }
+    }
+};
+
+int Bank::accNum = 1001;
+int Bank::index = 0;
+
+bool isAccountPresent(Bank bank)
+{
+    if (bank.index > 0)
+    {
+        return true;
+    }
+    return false;
+}
--- PROJECT/project.cpp
+++ PROJECT/project.cpp
@@ -1,166 +1,135 @@
 #include <iostream>
+#include "project-header.h"
 using namespace std;
-
-#define MIN_BALANCE 1500
-#define MAX_ACCOUNTS 100
-
-class Account
-{
-private:
-    int accNumber;
-    string name;
-    double balance;
-    int indexNum;
-
-public:
-    Account()
-    {
-        // DEFAULT CONSTRUCTOR
-    }
-    Account(int accNumber, string name, double balance)
-    {
-        // PARAMETERIZED CONSTRUCTOR
-        this->accNumber = accNumber;
-        this->name = name;
-        this->balance = balance;
-    }
-
-    // UTILITY FUNCTIONS
-    void deposit(int amount)
-    {
-        balance = balance + amount;
-    }
-    void withdraw(int amount)
-    {
-        balance = balance - amount;
-    }
-
-    // GETTERS AND SETTERS
-    int getAccNumber()
-    {
-        return accNumber;
-    }
-
-    string getName()
-    {
-        return name;
-    }
-
-    double getBalance()
-    {
-        return balance;
-    }
-
-    int getIndexNum()
-    {
-        return indexNum;
-    }
-
-    void setName(string name)
-    {
-        this->name = name;
-    }
-
-    void setIndexNum(int indexNum)
-    {
-        this->indexNum = indexNum;
-    }
-
-    void setBalance(int balance)
-    {
-        this->balance = balance;
-    }
-
-    void displayData()
-    {
-        cout << "ACCOUNT NUMBER : " << accNumber << endl;
-        cout << "ACCOUNT NAME : " << name << endl;
-        cout << "ACCOUNT BALANCE : " << balance << endl;
-        cout << "INDEX NUMBER : " << indexNum << endl;
-    }
-};
-class Bank
-{
-private:
-    // ACOUNTS
-    static int accNum;
-    static int index;
-    Account accounts[MAX_ACCOUNTS];
-
-public:
-    void createAccount(string name, double amount)
-    {
-        if (index < MAX_ACCOUNTS)
-        {
-            if (amount >= MIN_BALANCE)
-            {
-                Account accountObj(accNum, name, amount);
-                accountObj.setIndexNum(index);
-                accounts[index] = accountObj;
-                cout << "ACCOUNT CREATED SUCCESSFULLY !!!\n";
-                accountObj.displayData();
-                accNum++;
-                index++;
-            }
-            else
-            {
-                cout << "PLEASE ENTER AMOUNT MORE THAN " << MIN_BALANCE << " !!!" << endl;
-            }
-        }
-        else
-        {
-            cout << "CURRENTLY OUT OF SERVICE !!! \n";
-        }
-    }
-
-    Account getAccountByAccNum(int accountNumber)
-    {
-        // SEARCH FROM ARRAY
-        for (int i = 0; i < index; i++)
-        {
-            if (accounts[i].getAccNumber() == accountNumber)
-            {
-                return accounts[i];
-            }
-        }
-        Account dummyAccount;
-        dummyAccount.setIndexNum(-1);
-        return dummyAccount;
-    }
-
-    void depositAmount(double amount, int accountNumber)
-    {
-        Account existingAccount = getAccountByAccNum(accountNumber);
-        if (existingAccount.getIndexNum() == -1)
-        {
-            cout << "ACCOUNT WITH ACCOUNT NUMBER " << accountNumber << " DOESN'T EXISTS !!!\n";
-        }
-        else
-        {
-            existingAccount.deposit(amount);
-            int existingAccountIndex = existingAccount.getIndexNum();
-            accounts[existingAccountIndex] = existingAccount;
-            cout << "AMOUNT " << amount << " CREDITED SUCCESSFULLY AND THE AVAILABLE BALANCE IS " << existingAccount.getBalance() << endl;
-        }
-    }
-};
-int Bank::accNum = 1001;
-int Bank::index = 0;
 
 int main()
 {
     Bank bank;
-    bank.createAccount("VINIT", 3000);
-    bank.createAccount("SAMEER", 2000);
+    bool isActive = true;
 
-    cout << "FINDING ACCOUNTS BY ACCOUNT NUMBER : \n";
-    bank.getAccountByAccNum(1001).displayData();
+    int accNumber;
+    int choice;
+    double amount;
+    string name;
 
-    bank.depositAmount(3000, 1002);
-    cout << "FINDING ACCOUNTS BY ACCOUNT NUMBER : \n";
-    bank.getAccountByAccNum(1002).displayData();
+    while (isActive)
+    {
+        cout << "PLEASE SELECT THE AVAILABLE CHOICE :\n1. CREATE ACCOUNT\n2. SEARCH ACCOUNT\n3. WITHDRAW AMOUNT\n4. DEPOSIT AMOUNT\n5. TRANSFER AMOUNT\n6. EXIT\n";
+        cin >> choice;
+        fflush(stdin);
 
-    bank.depositAmount(2000, 1000);
-    cout << "FINDING ACCOUNTS BY ACCOUNT NUMBER : \n";
-    bank.getAccountByAccNum(1001).displayData();
+        switch (choice)
+        {
+        case 1:
+            cout << "PLEASE ENTER YOUR NAME : ";
+            getline(cin, name);
+            cout << "PLEASE ENTER AMOUNT, MINIMUM AMOUNT IS " << MIN_BALANCE << " : ";
+            cin >> amount;
+            bank.createAccount(name, amount);
+            break;
+        case 2:
+            if (isAccountPresent(bank))
+            {
+                cout << "PLEASE ENTER YOUR ACCOUNT NUMBER : ";
+                cin >> accNumber;
+                Account account = bank.getAccountByAccNum(accNumber);
+                if (account.getIndexNum() == -1)
+                {
+                    cout << "ACCOUNT WITH ACCOUNT NUMBER : " << accNumber << " DOESN'T EXISTS !!!\n";
+                }
+                else
+                {
+                    account.displayData();
+                }
+            }
+            else
+            {
+                cout << "FIRST CREATE A ACCOUNT !!!\n";
+            }
+            break;
+        case 3:
+            if (isAccountPresent(bank))
+            {
+                cout << "PLEASE ENTER YOUR ACCOUNT NUMBER : ";
+                cin >> accNumber;
+                Account account = bank.getAccountByAccNum(accNumber);
+                if (account.getIndexNum() == -1)
+                {
+                    cout << "ACCOUNT WITH ACCOUNT NUMBER : " << accNumber << " DOESN'T EXISTS !!!\n";
+                }
+                else
+                {
+                    cout << "PLEASE ENTER AMOUNT TO WITHDRAW : ";
+                    cin >> amount;
+                    bank.withdrawAmount(amount, accNumber);
+                }
+            }
+            else
+            {
+                cout << "FIRST CREATE A ACCOUNT !!!\n";
+            }
+            break;
+        case 4:
+            if (isAccountPresent(bank))
+            {
+                cout << "PLEASE ENTER YOUR ACCOUNT NUMBER : ";
+                cin >> accNumber;
+                Account account = bank.getAccountByAccNum(accNumber);
+                if (account.getIndexNum() == -1)
+                {
+                    cout << "ACCOUNT WITH ACCOUNT NUMBER : " << accNumber << " DOESN'T EXISTS !!!\n";
+                }
+                else
+                {
+                    cout << "PLEASE ENTER AMOUNT TO DEPOSIT : ";
+                    cin >> amount;
+                    bank.depositAmount(amount, accNumber);
+                }
+            }
+            else
+            {
+                cout << "FIRST CREATE A ACCOUNT !!!\n";
+            }
+            break;
+        case 5:
+            if (isAccountPresent(bank))
+            {
+                int fromAccountNumber, toAccountNumber;
+                cout << "PLEASE ENTER SOURCE ACCOUNT NUMBER : ";
+                cin >> fromAccountNumber;
+                Account account = bank.getAccountByAccNum(fromAccountNumber);
+                if (account.getIndexNum() == -1)
+                {
+                    cout << "ACCOUNT WITH ACCOUNT NUMBER " << fromAccountNumber << " DOESN'T EXISTS !!!\n";
+                }
+                else
+                {
+                    cout << "PLEASE ENTER DESTINATION ACCOUNT NUMBER : ";
+                    cin >> toAccountNumber;
+                    Account account = bank.getAccountByAccNum(toAccountNumber);
+                    if (account.getIndexNum() == -1)
+                    {
+                        cout << "ACCOUNT WITH ACCOUNT NUMBER " << toAccountNumber << " DOESN'T EXISTS !!!\n";
+                    }
+                    else
+                    {
+                        cout << "PLEASE ENTER AMOUNT TO TRANSFER : ";
+                        cin >> amount;
+                        bank.transferAmount(fromAccountNumber, toAccountNumber, amount);
+                    }
+                }
+            }
+            else
+            {
+                cout << "FIRST CREATE A ACCOUNT !!!\n";
+            }
+            break;
+        case 6:
+            cout << "THANKS FOR USING OUR BANK !!!\n";
+            isActive = false;
+            break;
+        default:
+            cout << "PLEASE SELECT THE CORRECT OPTION !!!\n";
+        }
+    }
 }
